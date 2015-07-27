178e11ddc381f2b6e064c353e9726ac3f1dc4b9c
Nebojsa Vislavski (Will) <nebojsa@vislavski.com>
2015-07-27 14:42:08 -0700

    another commit

--- source/app/controllers/index.js
+++ source/app/controllers/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var common = require('common-mixers').common;
-var IMAPClient = projectRequire('lib/imap-client');
+var IMAPClient = require('../lib/imap-client');
 
 /**
  * Setup controllers with express app.
@@ -9,34 +8,7 @@ var IMAPClient = projectRequire('lib/imap-client');
  */
 var setup = function(app) {
     app.post('/v1/verify', function(req, res) {
-        var validInput = common.validateInput(res, req.body, {
-            id : '/verify',
-            type: 'object',
-            properties: {
-                user: {type: 'string', required: true},
-                host: {type: 'string', required: true},
-                port: {type: 'number', required: true}
-            }
-        });
 
-        if (!validInput) {
-            logger.info('post /v1/verify - failed request validation. Request: [%j]', req);
-            return;
-        }
-
-        var imapClient = new IMAPClient(req.body);
-        imapClient.connect(function(err) {
-            if (err) {
-                return common.respondError(
-                    res,
-                    401,
-                    'Failed to authenticate account: ' + JSON.stringify(err),
-                    true
-                );
-            } else {
-                return common.respond(res, {success: true, connection: req.body}, true);
-            }
-        });
     });
 };
 
--- source/app/index.js
+++ source/app/index.js
@@ -4,15 +4,10 @@
 /* global GLOBAL */
 'use strict';
 
-GLOBAL.projectRequire = require('rfr');
-if (!/.*source\/app\/$/.test(projectRequire.root)) {
-    projectRequire.root = projectRequire.root + 'source/app';
-}
-
 var express = require('express');
 
 // Load our stuff.
-var config = projectRequire('config');
+var config = require('./config');
 var bodyParser = require('body-parser');
 var app = null;
 
@@ -34,13 +29,8 @@ var runApplication = function(callback) {
         port = GLOBAL.unitTestPort;
     }
 
+    console.log('Listening on port ', port);
     app.listen(port);
-    logger.debug('IS_IMAPCollector: Listening on port: ' + port);
-
-    var SyncManager = projectRequire('lib/sync-manager');
-    var syncManager = new SyncManager();
-    syncManager.startSQSListener();
-
     callback();
 };
 
@@ -49,29 +39,11 @@ var runApplication = function(callback) {
  * @return {[type]} [description]
  */
 var initialize = function() {
-
-    var dbConfig = config.database.email_caddie;
-
-    require('common-mixers').models(
-        dbConfig,
-        require('./models').setup,
-        function done(err) {
-            if (err) {
-                logger.error('Cannot initialize models. Existing process.', err);
-                process.exit();
-            }
-
-            logger.info('models are initialized');
-
-            runApplication(function done(err) {
-                if (err) {
-                    logger.error('Application cannot run.', err);
-                    process.exit();
-                }
-
-                logger.info('application is running');
-            });
-        });
+    runApplication(function done(err) {
+        if (err) {
+            process.exit();
+        }
+    });
 };
 
 initialize();
@@ -80,48 +52,6 @@ initialize();
  Catch exit status
  */
 process.on('exit', function(code) {
-    logger.error('Exit status:', code);
-});
-
-/**
- * Handle uncaught exceptions
- */
-process.on('uncaughtException', function(err) {
-    var log = err.stack;
-
-    logger.error('server crashed', log);
-    if (!config.emailCrash) {
-        logger.error('Won\'t send crash report, in test mode.');
-        return;
-    }
-
-    var nodemailer = require('nodemailer');
-    var sesTransport = require('nodemailer-ses-transport');
-    var transport = nodemailer.createTransport(sesTransport({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
-        rateLimit: 1 // do not send more than 1 message in a second
-    }));
-
-    var subject = '(' + process.env.NODE_ENV + ') AWS: IS_IMAPCollector - Crash Log';
-
-    var mailOptions = {
-        from: 'vikings@sugarcrm.com', // sender address
-        to: 'vikings@sugarcrm.com', // list of receivers
-        subject: subject, // Subject line
-        text: log
-    };
-
-    transport.sendMail(mailOptions, function(error, response) {
-        if (error) {
-            logger.error('Could not send crash mail ', error);
-        } else {
-            logger.error('Send crash mail: ' + response.message);
-        }
-    });
-
-    // If we exit straight away, the write log and send email operations wont have time to run
-    setTimeout (function() {process.exit(); }, 5000);
 });
 
 // Export app so integration tests can use it.
