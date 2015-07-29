d8c54a675b4e17604207ec93d2ff3fe796ad82e4
Nebojsa Vislavski (Will) <nebojsa@vislavski.com>
2015-07-29 13:41:57 -0700

    update

--- source/app/models/graph-node.js
+++ source/app/models/graph-node.js
@@ -1,10 +1,11 @@
 'use strict';
 
+
+
 // To http://localhost:7474/db/data with user "local" and pass "test"
-var dbLocal = require('seraph')({
-    user: 'neo4j',
-    pass: 'rootpass'
-});
+var dbLocal = require('seraph')({ server: "http://52.10.156.36:7474",
+                                   user: "neo4j",
+                                   pass: "rootpass" });
 
 var map = {};
 
@@ -32,10 +33,24 @@ exports.storeNode = function(email, name, callback) {
 
 exports.addRelationship = function(email1, email2, attributeName, attributeValue, callback) {
     var relObject = {};
-    relObject[attributeName] = attributeValue;
-    dbLocal.relate(map[email1], 'knows', map[email2], relObject, function(err, relationship) {
-        console.log('added relationship:', email1, ' -> ', email2);
-        map[email1 + '-' + email2] = relationship.id;
-        callback();
-    });
+    var key = email1 + '-' + email2;
+    var id = null;
+    if (map[key]) {
+        id = map[key];
+        // update rel with id
+        dbLocal.rel.read(id, function(err, relationship) {
+            relationship.properties[attributeName] = attributeValue;
+            dbLocal.rel.update(relationship, function() {
+                console.log('updated relationship');
+                callback();
+            });
+        });
+    } else {
+        relObject[attributeName] = attributeValue;
+        dbLocal.relate(map[email1], 'knows', map[email2], relObject, function(err, relationship) {
+            console.log('added relationship:', email1, ' -> ', email2);
+            map[email1 + '-' + email2] = relationship.id;
+            callback();
+        });
+    }
 };
